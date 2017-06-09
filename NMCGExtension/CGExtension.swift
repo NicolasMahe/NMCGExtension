205420d2966f6db2eb82a8396c6dc90cc3f67f0f
Nicolas Mahé <nicolas@mahe.me>
2017-06-09 16:00:09 +0700

    add + and - operator on CGPoint

--- NMCGExtension/CGExtension.swift
+++ NMCGExtension/CGExtension.swift
@@ -108,3 +108,21 @@ extension CGSize {
   
 }
 
+extension CGPoint {
+  
+  public static func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(
+      x: left.x + right.x,
+      y: left.y + right.y
+    )
+  }
+  
+  public static func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(
+      x: left.x - right.x,
+      y: left.y - right.y
+    )
+  }
+  
+}
+
